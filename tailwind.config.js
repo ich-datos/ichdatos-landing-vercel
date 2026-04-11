0f461a90c5a599ee36292d9a3855945e3595fb5e
Cozzich <francisco@ichdatos.com.ar>
2026-04-11 16:55:39 -0300

    Efecto preloader de carga

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}", // <-- ESTA LÍNEA ES CLAVE
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    // Si tenés carpetas viejas fuera de src/app, agregalas acá:
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
